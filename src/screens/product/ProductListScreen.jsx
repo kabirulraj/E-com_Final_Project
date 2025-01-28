c498f5667a326675e43ee0f159f592cec8d055ec
kabirulraj <kabirul2001@gmail.com>
2025-01-28 19:34:06 +0530

    fixed product list

--- src/screens/product/ProductListScreen.jsx
+++ src/screens/product/ProductListScreen.jsx
@@ -5,77 +5,7 @@ import { Link } from "react-router-dom";
 import ProductList from "../../components/product/ProductList";
 import { products } from "../../data/data";
 import Title from "../../components/common/Title";
-import { breakpoints, defaultTheme } from "../../styles/themes/default";
-import ProductFilter from "../../components/product/ProductFilter";
-
-const ProductsContent = styled.div`
-  grid-template-columns: 320px auto;
-  margin: 20px 0;
-
-  @media (max-width: ${breakpoints.xl}) {
-    grid-template-columns: 260px auto;
-  }
-
-  @media (max-width: ${breakpoints.lg}) {
-    grid-template-columns: 100%;
-    row-gap: 24px;
-  }
-`;
-
-const ProductsContentLeft = styled.div`
-  border: 1px solid rgba(190, 188, 189, 0.4);
-  border-radius: 12px;
-  box-shadow: rgba(0, 0, 0, 0.05) 0 10px 50px;
-  overflow: hidden;
-
-  @media (max-width: ${breakpoints.lg}) {
-    display: grid;
-  }
-`;
-
-const ProductsContentRight = styled.div`
-  padding: 16px 40px;
-
-  .products-right-top {
-    margin-bottom: 40px;
-    @media (max-width: ${breakpoints.lg}) {
-      margin-bottom: 24px;
-    }
-    @media (max-width: ${breakpoints.sm}) {
-      flex-direction: column;
-      row-gap: 16px;
-      align-items: flex-start;
-    }
-  }
-
-  .products-right-nav {
-    column-gap: 16px;
-    li {
-      a.active {
-        color: ${defaultTheme.color_purple};
-      }
-    }
-  }
-
-  @media (max-width: ${breakpoints.lg}) {
-    padding-left: 12px;
-    padding-right: 12px;
-  }
-
-  @media (max-width: ${breakpoints.sm}) {
-    padding-left: 0;
-    padding-right: 0;
-  }
-
-  .product-card-list {
-    grid-template-columns: repeat(auto-fill, repeat(290px, auto));
-  }
-
-  .product-card {
-    padding-left: 0;
-    padding-right: 0;
-  }
-`;
+import { breakpoints } from "../../styles/themes/default";
 
 const DescriptionContent = styled.div`
   .content-stylings {
@@ -95,11 +25,7 @@ const ProductListScreen = () => {
     <main className="page-py-spacing">
       <Container>
         <Breadcrumb items={breadcrumbItems} />
-        <ProductsContent className="grid items-start">
-          <ProductsContentLeft>
-            <ProductFilter />
-          </ProductsContentLeft>
-          <ProductsContentRight>
+        
             <div className="products-right-top flex items-center justify-between">
               <h4 className="text-xxl">Men & Women&apos;s Clothing</h4>
               <ul className="products-right-nav flex items-center justify-end flex-wrap">
@@ -116,8 +42,7 @@ const ProductListScreen = () => {
               </ul>
             </div>
             <ProductList products={products.slice(0, 12)} />
-          </ProductsContentRight>
-        </ProductsContent>
+          
       </Container>
       <Section>
         <Container>
